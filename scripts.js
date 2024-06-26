f6ec766f02844911607288aee28cfdbf00ba6ebc
Prince Amponsah <princekwes@gmail.com>
2024-06-26 00:27:54 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Add your API endpoint here
-var API_ENDPOINT = "https://rknsyuic85.execute-api.us-west-2.amazonaws.com/prod";
+var API_ENDPOINT = "";
 
 // AJAX POST request to save student data
 document.getElementById("savestudent").onclick = function(){
@@ -10,7 +10,7 @@ document.getElementById("savestudent").onclick = function(){
         "age": $('#age').val()
     };
     $.ajax({
-        url: "https://rknsyuic85.execute-api.us-west-2.amazonaws.com/prod",
+        url: API_ENDPOINT,
         type: 'POST',
         data:  JSON.stringify(inputData),
         contentType: 'application/json; charset=utf-8',
@@ -26,7 +26,7 @@ document.getElementById("savestudent").onclick = function(){
 // AJAX GET request to retrieve all students
 document.getElementById("getstudents").onclick = function(){  
     $.ajax({
-        url: "https://rknsyuic85.execute-api.us-west-2.amazonaws.com/prod",
+        url: API_ENDPOINT,
         type: 'GET',
         contentType: 'application/json; charset=utf-8',
         success: function (response) {
